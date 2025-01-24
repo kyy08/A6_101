004c67131cb2637f0397fdfb506bb9dd11c81222
kyy08 <kyyelzz1@gmail.com>
2025-01-24 14:23:40 +0700

    memanggil acara app di mainactivity

---
+++ app/src/main/java/com/example/finalprojectpam/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.finalprojectpam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.finalprojectpam.ui.theme.FinalProjectPamTheme
+import com.example.finalprojectpam.ui.viewmodel.AcaraApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FinalProjectPamTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    AcaraApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FinalProjectPamTheme {
+        Greeting("Android")
+    }
+}
