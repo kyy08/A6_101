024cf250bb2123a0b2867bcfe1a6465676090a6c
kyy08 <kyyelzz1@gmail.com>
2025-01-27 18:19:41 +0700

    mengupdate VendorRepository

--- app/src/main/java/com/example/finalprojectpam/repository/VendorRepository.kt
+++ app/src/main/java/com/example/finalprojectpam/repository/VendorRepository.kt
@@ -2,33 +2,35 @@ package com.example.finalprojectpam.repository
 
 import com.example.finalprojectpam.model.Vendor
 import com.example.finalprojectpam.service.VendorService
-import java.io.IOException
 
 interface VendorRepository {
     suspend fun getVendor(): List<Vendor>
     suspend fun insertVendor(vendor: Vendor)
-    suspend fun updateVendor(id: String, vendor: Vendor)
-    suspend fun deleteVendor(id: String)
-    suspend fun getVendorById(id: String): Vendor
+    suspend fun updateVendor(id_vendor: String, vendor: Vendor)
+    suspend fun deleteVendor(id_vendor: String)
+    suspend fun getVendorById(id_vendor: String): Vendor
 }
 
-class NetworkVendorRepository(
-    private val vendorApiService: VendorService
-) : VendorRepository {
+class NetworkVendorRepository(private val vendorService: VendorService)
+    : VendorRepository
+{
+    override suspend fun getVendor(): List<Vendor> = vendorService.getVendor()
+
     override suspend fun insertVendor(vendor: Vendor) {
-        vendorApiService.insertVendor(vendor)
+        vendorService.insertVendor(vendor)
     }
 
-    override suspend fun updateVendor(id: String, vendor: Vendor) {
-        vendorApiService.updateVendor(id, vendor)
+    override suspend fun updateVendor(id_vendor: String, vendor: Vendor) {
+        vendorService.updateVendor(id_vendor, vendor)
     }
 
-    override suspend fun deleteVendor(id: String) {
+    override suspend fun deleteVendor(id_vendor: String) {
         try {
-            val response = vendorApiService.deleteVendor(id)
+            val response = vendorService.deleteVendor(id_vendor)
             if (!response.isSuccessful) {
-                throw IOException("Failed to delete vendor. HTTP Status code: ${response.code()}")
-            } else {
+                throw Exception("Failed to delete vendor. HTTP Status Code: ${response.code()}")
+            }
+            else{
                 response.message()
                 println(response.message())
             }
@@ -37,13 +39,7 @@ class NetworkVendorRepository(
         }
     }
 
-    override suspend fun getVendor(): List<Vendor> = vendorApiService.getVendor()
-
-    override suspend fun getVendorById(id: String): Vendor {
-        try {
-            return vendorApiService.getVendorById(id)
-        } catch (e: IOException) {
-            throw IOException("Failed to fetch vendor with Id: $id. Network error occurred.", e)
-        }
+    override suspend fun getVendorById(id_vendor: String): Vendor {
+        return vendorService.getVendorById(id_vendor)
     }
 }
